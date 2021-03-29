b8c38007f557fcfc99485e66cc56cfa2ad43b488
Sean Melody <seandmelody@gmail.com>
2021-03-28 19:51:31 -0700

    QandA added

--- src/Pages/QandA/QandA.js
+++ src/Pages/QandA/QandA.js
@@ -5,6 +5,95 @@ const QandA = () => {
         <div>
             <h1>COVID and Q & A</h1>
             <img src="./images/Pandemic.jpg" alt="Pandemic" className="sf img-thumbnail col-md-4"></img>
+            <br></br>
+            <div className="container">
+                <h3 className="mx-auto">COVID</h3>
+                <div className="row">
+                    <p className="col-md-8 mx-auto">
+                        We will keep updating this page with information as it becomes available,
+                        but we are confident and have our fingers and toes crossed that all restrictions
+                        will be lifted in time for our wedding!!
+                    </p>
+                </div>
+                <br></br>
+                <h3 className="mx-auto">RSVP Deadline</h3>
+                <div className="row">
+                    <p className="col-md-8 mx-auto">
+                        Deadline to RSVP is August 15th, but we would love to hear from you as soon as possible.
+                    </p>
+                </div>
+                <br></br>
+                <h3 className="mx-auto">Can I bring a date?</h3>
+                <div className="row">
+                    <p className="col-md-8 mx-auto">
+                        Please check your invite for your +1. We have a very strict limit to how many people can attend,
+                        but if you have any questions, please reach out to us: CookerlyMelody@gmail.com
+                    </p>
+                </div>
+                <br></br>
+                <h3 className="mx-auto">Are kids welcome?</h3>
+                <div className="row">
+                    <p className="col-md-8 mx-auto">
+                        As much as we love your little ones, we will not be including them in the ceremony or reception,
+                        due to our strict limit on how many people we can have. However, we recognize that some of you
+                        will be traveling with your kids, so please know they are welcome at the Sunday beach party!
+                    </p>
+                </div>
+                <br></br>
+                <h3 className="mx-auto">What will the weather be like?</h3>
+                <div className="row">
+                    <p className="col-md-8 mx-auto">
+                        October has the best weather for San Francisco, you can expect mid 70 degree sunny days,
+                        but be aware that it could get a little chilly at the wedding under the Redwood trees.
+                    </p>
+                </div>
+                <br></br>
+                <h3 className="mx-auto">Where should I park?</h3>
+                <div className="row">
+                    <p className="col-md-8 mx-auto">
+                        There is limited parking at Stern Grove, but there are two small lots if you are feeling lucky,
+                        or you can park on the street, though be aware of the San Francisco hills.
+                    </p>
+                </div>
+                <br></br>
+                <h3 className="mx-auto">Will the wedding be indoors or outdoors?</h3>
+                <div className="row">
+                    <p className="col-md-8 mx-auto">
+                        The ceremony will be outdoors under the redwoods, along with dinner.
+                        Though we will have dancing inside
+                    </p>
+                    <br></br>
+                </div>
+                <h3 className="mx-auto">What kind of shoes should/shouldn't I wear?</h3>
+                <div className="row">
+                    <p className="col-md-8 mx-auto">
+                        We politely ask that you do not wear stiletto heels. Or any tall heels. There are a bunch of
+                        grassy/dirt areas that you have to walk in so we ask that you wear chunky heels or sandals.
+                        Also we want everyone's feet to be comfortable
+                    </p>
+                </div>
+                <br></br>
+                <h3 className="mx-auto">Is it okay to take pictures with our phones and cameras during the wedding?</h3>
+                <div className="row">
+                    <p className="col-md-8 mx-auto">
+                        Yes! We would love for you to take photos and share them.
+                        However, please refrain from taking photos during the ceremony.
+                    </p>
+                </div>
+                <br></br>
+                <h3 className="mx-auto">Have an additional question?</h3>
+                <div className="row">
+                    <p className="col-md-8 mx-auto">
+                        Please reach out to our dedicated team answering emails at: CookerlyMelody@gmail.com
+                    </p>
+                </div>
+
+
+
+            </div>
+
+
+
         </div>
     )
 }
